5e77be61391008cbaa65e72e9cc67ff19f96dbdf
thlindustries <thlindustries23@gmail.com>
2023-04-04 15:53:54 -0300

    feat: adding info to the api

--- src/pages/api/info.ts
+++ src/pages/api/info.ts
@@ -3,6 +3,7 @@ export default function handler(req, res): void {
     info: {
       playstoreAppUri:
         'https://play.google.com/store/apps/details?id=com.futebolinterior',
+      appleStoreUri: 'https://thlinc.com',
     },
   });
 }
