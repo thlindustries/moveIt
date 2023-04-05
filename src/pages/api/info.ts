615d09f7a0f883a3fb1fc432812a7a652aa4fe95
thlindustries <thlindustries23@gmail.com>
2023-04-05 18:30:39 -0300

    toggle

--- src/pages/api/info.ts
+++ src/pages/api/info.ts
@@ -4,7 +4,7 @@ export default function handler(req, res): void {
       playstoreAppUri:
         'https://play.google.com/store/apps/details?id=com.futebolinterior',
       appleStoreUri: 'https://thlinc.com',
-      showAccessibilityFeat: false,
+      showAccessibilityFeat: true,
     },
   });
 }
