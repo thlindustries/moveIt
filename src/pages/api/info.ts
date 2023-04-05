dcc2f5893906fb77cddadf05800f106313626237
thlindustries <thlindustries23@gmail.com>
2023-04-05 09:01:44 -0300

    Refactor: adding accessibility functionality, fixing 'pos/pre-jogo' btn, fixing hour and date format, adding stadium name

--- src/pages/api/info.ts
+++ src/pages/api/info.ts
@@ -4,6 +4,7 @@ export default function handler(req, res): void {
       playstoreAppUri:
         'https://play.google.com/store/apps/details?id=com.futebolinterior',
       appleStoreUri: 'https://thlinc.com',
+      showAccessibilityFeat: false,
     },
   });
 }
