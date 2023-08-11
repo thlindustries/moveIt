a1d3db10ee92e1bd5676ad7baffa13fb48453a59
thlindustries <thlindustries23@gmail.com>
2023-08-11 10:49:43 -0300

    feat: adding portifolio api

---
+++ src/pages/api/services.ts
@@ -0,0 +1,200 @@
+export default function handler(req, res): void {
+  res.status(200).json({
+    services: {
+      'pt-br': [
+        {
+          name: 'Gestão de produtos',
+          items: [
+            {
+              id: '634f9021-d32a-4471-bcee-052cb5d989a9',
+              text:
+                'Contato direto com o cliente para realização do estudo de requisitos.',
+            },
+            {
+              id: '39f76539-5f95-484c-82e5-7dd932cde126',
+              text:
+                'Gerenciamento de equipe baseado em Scrum para otimizar as entregas e cumprir o cronograma.',
+            },
+          ],
+        },
+        {
+          name: 'Desenvolvimento web',
+          items: [
+            {
+              id: '467801b7-ffea-4e49-a866-b28d1a9a5907',
+              text:
+                'E-commerces com otimização SEO e melhores práticas de mercado.',
+            },
+            {
+              id: '3aa3aa27-b57a-4588-8fb6-d12e531b6cea',
+              text: 'Otimização da arquitetura de design para escalabilidade.',
+            },
+            {
+              id: 'ca36341b-2e32-49cd-8828-b0f45a46c0bf',
+              text:
+                'Frameworks mais usados para e-commerces como gatsby e nextjs.',
+            },
+            {
+              id: '2831a06d-3b47-4ee1-a9b9-b7340dbd778a',
+              text:
+                'Desenvolvimento multiplataforma usando aplicativos web react-native e responsivos.',
+            },
+            {
+              id: '42fbc71b-3756-4d10-816e-6b45eeda5478',
+              text:
+                'Desenvolvimento de bibliotecas, CI/CD, testes unity&E2E e +.',
+            },
+          ],
+        },
+        {
+          name: 'Plataforma de gerenciamento de conteúdos',
+          items: [
+            {
+              id: '90cc3042-81ee-4fef-a25d-84b44f42abc3',
+              text:
+                'Plataformas projetadas para gerenciar o conteúdo do seu site. (listra, do zero, etc...)',
+            },
+            {
+              id: '09de7d36-a0ee-4293-85e3-11eb3f77d4ed',
+              text:
+                'Nenhum código para temas totalmente personalizados. (Você pode encontrar um pequeno ovo de páscoa nesta página :D)',
+            },
+            {
+              id: '70edc6fa-64c7-4cbe-874b-d2a1f1d5bcf0',
+              text: 'Confiabilidade, fácil de usar e totalmente documentado.',
+            },
+          ],
+        },
+      ],
+      'en-us': [
+        {
+          name: 'Product management',
+          items: [
+            {
+              id: 'd76f04af-23c2-4679-b9d1-9a81044ceb09',
+              text:
+                'Direct contact with the customer to carry out the requirements study.',
+            },
+            {
+              id: '68b80c61-45df-48eb-87e5-4920915769fd',
+              text:
+                'Scrum based team management to optimize deliveries and meet the schedule.',
+            },
+          ],
+        },
+        {
+          name: 'Web Development',
+          items: [
+            {
+              id: '925cbca5-576c-492b-86f8-fa51ee22d5bf',
+              text:
+                'E-commerces with SEO optimization and best market pratices.',
+            },
+            {
+              id: 'ec587c3e-15e7-4835-ba84-1652a84cf3b6',
+              text: 'Design architecture optimization for scalability.',
+            },
+            {
+              id: '447b2a57-a811-46d4-b5f6-2f15e01e2851',
+              text:
+                'Most used frameworks for e-commerces like gatsby and nextjs.',
+            },
+            {
+              id: 'fd7da56e-a74c-4dcc-9df4-d3e399ae7dfe',
+              text:
+                'Cross platform development using react-native and responsive web applications.',
+            },
+            {
+              id: '3bff9574-9b29-4de1-96ed-cd600d3441b1',
+              text: 'Library development, CI/CD, unity&E2E tests and +.',
+            },
+          ],
+        },
+        {
+          name: 'Content management platforms',
+          items: [
+            {
+              id: '16f9f105-b22e-4c73-adea-01e7ca14aef4',
+              text:
+                'Platforms designed to manage your website content. (stripe, from scratch, etc...)',
+            },
+            {
+              id: 'd997f159-833a-48aa-86c1-2374b64e95d1',
+              text:
+                'No code for fully customized themes. (You may find a little easter egg on this page :D)',
+            },
+            {
+              id: 'dc0a3093-7400-49ac-86ad-6f0b7410e9f4',
+              text: 'Reliability, easy to use and fully documented.',
+            },
+          ],
+        },
+      ],
+      'es-sp': [
+        {
+          name: 'Gestión de productos',
+          items: [
+            {
+              id: 'f622e011-f20e-4faf-8435-4f1ef175a0c8',
+              text:
+                'Contacto directo con el cliente para realizar el estudio de requisitos.',
+            },
+            {
+              id: 'd6477465-d939-428c-8ea8-0f2321064dc9',
+              text:
+                'Gestión de equipos basada en Scrum para optimizar las entregas y cumplir con el cronograma.',
+            },
+          ],
+        },
+        {
+          name: 'Desarrollo web',
+          items: [
+            {
+              id: 'a3c089aa-8bf8-4a1f-81c1-dc1d3bc75cc5',
+              text:
+                'E-commerces con optimización SEO y mejores prácticas de mercado.',
+            },
+            {
+              id: '05c8fd0f-0a88-4b46-83c0-e5b718faafad',
+              text:
+                'Optimización de la arquitectura de diseño para la escalabilidad.',
+            },
+            {
+              id: 'a22f77d1-6c71-4e86-9f0a-a042502042ad',
+              text:
+                'Frameworks más utilizados para e-commerces como gatsby y nextjs.',
+            },
+            {
+              id: 'a2574fe4-490e-45f9-97de-8a558a1b2a31',
+              text:
+                'Desarrollo multiplataforma utilizando aplicaciones web nativas y receptivas.',
+            },
+            {
+              id: 'd1ab1112-53e0-4c0a-897f-a4be8aab0f68',
+              text: 'Desarrollo de librería, CI/CD, pruebas unity&E2E y +.',
+            },
+          ],
+        },
+        {
+          name: 'Content management platforms',
+          items: [
+            {
+              id: '8487ce7c-a2d3-46dc-8c00-30b143f9bf83',
+              text:
+                'Plataformas diseñadas para administrar el contenido de su sitio web. (stripe, desde cero, etc...)',
+            },
+            {
+              id: '5bce72c8-efbb-41be-b8a9-b3be071ba75f',
+              text:
+                'Sin código para temas totalmente personalizados. (Puede encontrar un pequeño huevo de Pascua en esta página: D)',
+            },
+            {
+              id: '097a1fa7-41f8-416e-861a-0dab38e857eb',
+              text: 'Fiabilidad, fácil de usar y totalmente documentado.',
+            },
+          ],
+        },
+      ],
+    },
+  });
+}
---
+++ src/pages/api/testimonials.ts
@@ -0,0 +1,132 @@
+export default function handler(req, res): void {
+  res.status(200).json({
+    testimonials: {
+      'pt-br': [
+        {
+          id: 'f4b074e2-3769-11ed-a261-0242ac120002',
+          name: 'Gabriel Matos Marconato',
+          title: 'Civil Engineer',
+          description:
+            'Uma das pessoas mais inteligentes e gente boa que conheço. Muito capacitada em diversas áreas e focado nos objetivos.',
+          image: 'https://i.imgur.com/MyqqwX3.jpg',
+        },
+        {
+          id: '86686ea3-6bb4-4659-a9c7-7bad83a1fc82',
+          name: 'Jorge Afonso',
+          title: "DMS Systems's CEO",
+          description:
+            'Saudades de ter você no squad cara! Aprendi demais com você, ainda mais que na faculdade! Sem dúvida, uma pessoa impar. O famoso thiagão que salva os projetos hahaha Foi uma honra fritar a cabeça resolvendo problemas e bebendo muito café com você, que um dia a gente volte a trabalhar juntos em um projeto daqueles que a gente gosta, beeem complexo para queimar a cabeça! Você vai chegar longe cara, continue trabalhado que você ainda vai revolucionar muito a nosso mundo! Grande abraço e todo o sucesso!',
+          image: 'https://i.imgur.com/rbpuNRh.jpg',
+        },
+        {
+          id: '96ef6eb2-bd15-441a-b47d-4a7ef7a6d393',
+          name: 'Wilson Lima',
+          title: 'Journalist',
+          description:
+            'O que mais admiro no Thiagão é a inteligência, a rapidez com que entende as dores e necessidades dos clientes. Não é necessário explicar várias vezes o que vc tá procurando. Dada sua perspicácia e experiência, Thiagão apresenta a melhor solução, com eficácia, agilidade e ótimo suporte, tudo dentro de um orçamento coerente. Por tudo isso eu recomendo esse cara.',
+          image: 'https://i.imgur.com/HzyFJGY.jpg',
+        },
+        {
+          id: '352cde3c-99c4-454c-8881-4aa9d3a02795',
+          name: 'Alexandre Cunha Almeida Leite',
+          title: 'Fisioterapeuta',
+          description:
+            'Excelente profissional, atencioso e competente. Ele fez um ótimo trabalho.',
+          image: 'https://i.imgur.com/dJfq5qS.jpg',
+        },
+        {
+          id: '770994fa-f83b-46c4-928c-63780e37b107',
+          name: 'Daniel Viola',
+          title: "CEO Agência de Marketing Digital Bro's",
+          description:
+            'Trabalhei em diversos projeto com o Thiagao e as principais características posso destacar pontualidade, comunicação e profissionalismo. Super recomendo, um dos top no mercado!',
+          image: 'https://i.imgur.com/2YnXXGu.jpg',
+        },
+      ],
+      'en-us': [
+        {
+          id: '8fab9e8d-015e-486a-97de-2e017939c350',
+          name: 'Gabriel Matos Marconato',
+          title: 'Civil Engineer',
+          description:
+            'One of the smartest and nicest people I know. Very capable in several areas and focused on goals.',
+          image: 'https://i.imgur.com/MyqqwX3.jpg',
+        },
+        {
+          id: '794b2225-b63c-4d5c-8626-f483bb489f7c',
+          name: 'Jorge Afonso',
+          title: "DMS Systems's CEO",
+          description:
+            "Miss having you on the squad man! I learned a lot from you, even more than in college! Undoubtedly, an odd person. The famous thiagão who saves projects hahaha It was an honor to fry our heads solving problems and drinking a lot of coffee with you, that one day we will work together again on a project that we like, very complex to burn our heads! You'll get far, man, keep working, you're going to revolutionize our world a lot! Big hugs and all the success!",
+          image: 'https://i.imgur.com/rbpuNRh.jpg',
+        },
+        {
+          id: '6c90286f-577f-4439-b5a8-3b55e9f3ad6f',
+          name: 'Wilson Lima',
+          title: 'Journalist',
+          description:
+            'What I admire most about Thiagão is his intelligence, the speed with which he understands the pain and needs of his clients. It is not necessary to explain several times what you are looking for. Given his acumen and experience, Thiagão presents the best solution, with efficiency, agility and great support, all within a consistent budget. For all that I recommend this guy.',
+          image: 'https://i.imgur.com/HzyFJGY.jpg',
+        },
+        {
+          id: 'cc45fd60-255f-4fff-b9af-d70799b10d2c',
+          name: 'Alexandre Cunha Almeida Leite',
+          title: 'Physiotherapist',
+          description:
+            'Excellent professional, attentive and competent. He did a great job.',
+          image: 'https://i.imgur.com/dJfq5qS.jpg',
+        },
+        {
+          id: '1650bd8e-5934-4286-b916-5dff2368cdd0',
+          name: 'Daniel Viola',
+          title: "Bro's publicity CEO",
+          description:
+            'I worked on several projects with Thiagao and the main characteristics I can highlight is punctuality, communication and professionalism. I highly recommend it, one of the best on the market!',
+          image: 'https://i.imgur.com/2YnXXGu.jpg',
+        },
+      ],
+      'es-sp': [
+        {
+          id: 'a8c79c4d-b8c9-4455-a4d3-c8a79a8ecb23',
+          name: 'Gabriel Matos Marconato',
+          title: 'Civil Engineer',
+          description:
+            'Una de las personas más inteligentes y agradables que conozco. Bien entrenado en varias áreas y enfocado en nuestros objetivos.',
+          image: 'https://i.imgur.com/MyqqwX3.jpg',
+        },
+        {
+          id: 'fe7d0d31-a6e4-4ec6-8a2e-83c742d0b4f5',
+          name: 'Jorge Afonso',
+          title: "DMS Systems's CEO",
+          description:
+            '¡Extraño tenerte en el escuadrón! Aprendí mucho de ti, ¡incluso más que en la universidad! Sin duda, una persona rara. El famoso thiagão que salva proyectos jajaja Fue un honor freírnos la cabeza resolviendo problemas y tomando mucho café contigo, que algún día volveremos a trabajar juntos en un proyecto que nos gusta, muy complejo para quemarnos la cabeza! Llegarás lejos, tío, sigue trabajando, ¡vas a revolucionar mucho nuestro mundo! Un fuerte abrazo y todo el éxito!',
+          image: 'https://i.imgur.com/rbpuNRh.jpg',
+        },
+        {
+          id: 'a67aca23-c5e3-460a-92ca-951d4bdd561b',
+          name: 'Wilson Lima',
+          title: 'Journalist',
+          description:
+            'Lo que más admiro de Thiagão es su inteligencia, la rapidez con la que comprende el dolor y las necesidades de sus clientes. No es necesario que expliques varias veces lo que buscas. Dada su perspicacia y experiencia, Thiagão presenta la mejor solución, con eficiencia, agilidad y gran soporte, todo dentro de un presupuesto consistente. Por todo eso recomiendo a este chico.',
+          image: 'https://i.imgur.com/HzyFJGY.jpg',
+        },
+        {
+          id: '5d2f628f-0b44-4845-8308-f73a8049f8b8',
+          name: 'Alexandre Cunha Almeida Leite',
+          title: 'Fisioterapeuta',
+          description:
+            'Excelente profesional, atento y competente. Él hizo un gran trabajo.',
+          image: 'https://i.imgur.com/dJfq5qS.jpg',
+        },
+        {
+          id: '9397b715-ff28-4f3e-a2fd-c84e36800726',
+          name: 'Daniel Viola',
+          title: ' - ',
+          description:
+            'Trabalhei em diversos projeto com o Thiagao y as principais caracteristicas posso destacan pontualidade, comunicação y profissionalism. Súper recomendado, um dos top no mercado!',
+          image: 'https://i.imgur.com/2YnXXGu.jpg',
+        },
+      ],
+    },
+  });
+}
