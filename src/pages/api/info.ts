e7535f4d4f13dda02603f2f87cb5080799c4dad7
Thiago Lorente Kraetzer <tkraetzer@Thiagos-MacBook-Pro.local>
2023-08-15 21:55:06 -0300

    test

--- src/pages/api/info.ts
+++ src/pages/api/info.ts
@@ -8,7 +8,7 @@ export default function handler(req, res): void {
           'https://play.google.com/store/apps/details?id=com.futebolinterior',
         appleStoreUri: 'https://thlinc.com',
         showAccessibilityFeat: false,
-        showNewBottomTabs: true,
+        showNewBottomTabs: false,
         scoreRefreshDelay: 5,
       },
     });
