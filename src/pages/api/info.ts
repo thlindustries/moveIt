f34eb8ffc3871884c89567e334ef16e276136fdd
Thiago Lorente Kraetzer <tkraetzer@Thiagos-MacBook-Pro.local>
2023-10-20 10:10:25 -0300

    feat: adding liveGameRefreshToggle

--- src/pages/api/info.ts
+++ src/pages/api/info.ts
@@ -10,6 +10,7 @@ export default function handler(req, res): void {
         showAccessibilityFeat: false,
         showNewBottomTabs: false,
         scoreRefreshDelay: 5,
+        disableLiveGamesRefresh: true,
       },
     });
   });
